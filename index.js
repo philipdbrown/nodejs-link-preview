84575acbb12128a92c4d015f7ffb8c68b6e7bc2b
Philip Brown <philip@supplyhog.com>
2021-02-27 10:24:39 -0500

    Port Environment Variable | Clean Up
    Allow the port variable to be set by an environment variable and not
    within the code.  If the environment variable PREV_LINK_PORT is not
    set, the process will exit.
    Added semicolons to the end of commands.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const Regex = require('./Regex.js');
 const Media = require('./Media.js');
 const Helper = require('./Helper.js');
 const app = express();
+const port = process.env.PREV_LINK_PORT;
+
+if(port == undefined || port < 3000) {
+    console.log('ERROR:  Missing PREV_LINK_PORT environment variable.  This will set the port number.\n');
+    process.exit(1);
+}
 
 app.get('/preview-link', (req, res) => {
 
@@ -18,29 +24,29 @@ app.get('/preview-link', (req, res) => {
             description: '',
             image: '',
             video: '',
-        }
+        };
 
         if (Helper.isImage(url)) {
-            returnRes.image = url
-            res.send(returnRes)
+            returnRes.image = url;
+            res.send(returnRes);
         } else {
             request(url, function (error, response, body) {
                 if (!error) {
-                    var $ = cheerio.load(body)
+                    var $ = cheerio.load(body);
 
                     returnRes.title = $('title').text();
                     let og_title = $('meta[property="og:title"]').attr('content');
-                    if( og_title ) {
-                        returnRes.title = og_title
+                    if (og_title) {
+                        returnRes.title = og_title;
                     }
 
                     //desc
                     returnRes.description = $('meta[name="description"]').attr('content') || '';
                     let og_description = $('meta[property="og:description"]').attr('content');
                     if (og_description) {
-                        returnRes.description = og_description
+                        returnRes.description = og_description;
                     }
-                    
+
                     //managing images
                     let og_image = $('meta[property="og:image"]').attr('content');
                     if (og_image) {
@@ -50,12 +56,12 @@ app.get('/preview-link', (req, res) => {
                     //check for media stuff
                     returnRes.video = Media.getMedia(url);
                 } else {
-                    returnRes.error = error
+                    returnRes.error = error;
                 }
-                res.send(returnRes)
+                res.send(returnRes);
             });
         }
     }
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`NODEJS-LINK-PREVIEW app listening on port ${port}!`));
