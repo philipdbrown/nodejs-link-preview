38cfb0b1d4798955eece93e7d4052011cb1812e4
Philip Brown <philip@supplyhog.com>
2021-02-26 23:00:55 -0500

    Refactored for Simplification | Obtain youtu.be Embed File

--- Media.js
+++ Media.js
@@ -1,53 +1,54 @@
 module.exports = {
 	getMedia(pageUrl) {
-		media = false;
-		if (pageUrl.indexOf('youtube.com') != -1) {
-			media = this.mediaYoutube(pageUrl);
-		} else if (pageUrl.indexOf('ted.com') != -1) {
-			media = this.mediaTED(pageUrl);
-		} else if (pageUrl.indexOf('vimeo.com') != -1) {
-			media = this.mediaVimeo(pageUrl);
-		} else if (pageUrl.indexOf('dailymotion.com') != -1) {
-			media = this.mediaDailymotion(pageUrl);
-		}
-		return media;
+        if (pageUrl.indexOf('youtube.com') != -1) return this.mediaYoutube(pageUrl);
+        if (pageUrl.indexOf('youtu.be') != -1) return this.mediaYoutu(pageUrl);
+		if (pageUrl.indexOf('ted.com') != -1) return this.mediaTED(pageUrl);
+		if (pageUrl.indexOf('vimeo.com') != -1) return this.mediaVimeo(pageUrl);
+		if (pageUrl.indexOf('dailymotion.com') != -1) return this.mediaDailymotion(pageUrl);
+		return false;
 	},
 	mediaYoutube(url) {
-		media = '';
 		let tokens = url.split('v=')
 		if (tokens[1]) {
-			media = "http://www.youtube.com/embed/" + tokens[1]
+			return "http://www.youtube.com/embed/" + tokens[1]
 		}
-		return media;
-	},
+		return '';
+    },
+    mediaYoutu(url) {
+        url = url.split('/')
+		if (url.length > 0) {
+			token = url[url.length - 1];
+			if (token.length > 0) {
+				return "http://www.youtube.com/embed/" + token;
+			}
+		}
+		return '';
+
+    },
 	mediaTED(url) {
 		url = url.split('/')
-		media = '';
 		if (url.length > 0) {
 			url = url[url.length - 1];
 			url = url.split('?');
 			if (url.length > 0) {
-				media = 'https://embed-ssl.ted.com/talks/' + url[0]
+				return 'https://embed-ssl.ted.com/talks/' + url[0]
 			}
 		}
-		return media;
+		return '';
 	},
 	mediaVimeo(url) {
-		url = url.replace('https://', '');
-		url = url.replace('http://', '');
+		url = url.replace(/(^\w+:|^)\/\//, '');
 		let breakUrl = url.split('/')
-		media = '';
 		if (breakUrl[1] != "") {
-			media = 'http://player.vimeo.com/video/' + breakUrl[1];
+			return 'http://player.vimeo.com/video/' + breakUrl[1];
 		}
-		return media;
+		return '';
 	},
 	mediaDailymotion(url) {
-		media = '';
 		token = url.split('/');
 		if (token.length) {
-			media = 'http://www.dailymotion.com/embed/video/' + token[token.length - 1];
+			return 'http://www.dailymotion.com/embed/video/' + token[token.length - 1];
 		}
-		return media;
-	}
+		return '';
+    }
 }
